e5739e02ef92ce9e50b9b82532b4d408982bc91a
victorypa <victorypa1992@yandex.ru>
2018-10-30 18:38:52 +0300

    fcommit

---
+++ src/js/common.js
@@ -0,0 +1,12 @@
+    $(document).ready(function () {
+      $('.sidenav').sidenav();
+      $('.materialboxed').materialbox();
+      $('.parallax').parallax();
+      $('.modal').modal();
+      $('.tabs').tabs();
+      $('.datepicker').datepicker({
+        disableWeekends: true,
+      });
+      $('.tooltipped').tooltip();
+      $('.scrollspy').scrollSpy();
+    })
